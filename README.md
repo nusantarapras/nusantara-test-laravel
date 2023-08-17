2b27f5d3f54b3b62c6de96adeaea8a9b8574d070
nusantarapras <pras.elang@gmail.com>
2023-08-17 08:48:25 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+Requirement :
+    - PHP 8.x keatas
+    - Mysql
+    - Laravel 9 keatas
+
+Tahapan instalasi: 
+    - composer install
+    - npm install
+    - php artisan ke:generate
+    - php artisan migrate
+    - php artisan db:seed
+    - npm run build
+    - php artisan serve
+
+Credential :
+    user : admin@example.com
+    pass : password
---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,102 @@
+<script setup>
+import { Head, Link } from "@inertiajs/vue3";
+import ApplicationLogo from "@/Components/ApplicationLogo.vue";
+
+defineProps({
+  canLogin: Boolean,
+  canRegister: Boolean,
+  laravelVersion: String,
+  phpVersion: String,
+});
+</script>
+
+<template>
+  <Head title="Welcome" />
+
+  <div
+    class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white"
+  >
+    <div v-if="canLogin" class="sm:fixed sm:top-0 sm:right-0 p-6 text-right">
+      <Link
+        v-if="$page.props.auth.user"
+        :href="route('dashboard')"
+        class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
+        >Dashboard</Link
+      >
+
+      <template v-else>
+        <Link
+          :href="route('login')"
+          class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
+          >Log in</Link
+        >
+
+        <Link
+          v-if="canRegister"
+          :href="route('register')"
+          class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
+          >Register</Link
+        >
+      </template>
+    </div>
+
+    <div class="max-w-7xl mx-auto p-6 lg:p-8">
+      <div class="flex justify-center">
+        <ApplicationLogo  class="w-20 h-20 fill-current text-cyan-400"></ApplicationLogo>
+      </div>
+
+      <div class="mt-16">
+        <div class="grid grid-cols-1 md:grid-cols-1 gap-6 lg:gap-8">
+
+
+          <div
+            class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500"
+          >
+            <div>
+
+              <h2
+                class="mt-6 text-xl font-semibold text-gray-900 dark:text-white"
+              >
+                Test Programmer Laravel
+              </h2>
+
+              <p
+                class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed"
+              >
+              By. Nusantara Prasetya Yudha Putra Wijaya
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div
+        class="flex justify-center mt-16 px-6 sm:items-center sm:justify-between"
+      >
+        <div
+          class="text-center text-sm text-gray-500 dark:text-gray-400 sm:text-left"
+        >
+          <div class="flex items-center gap-4">
+          </div>
+        </div>
+
+        <div
+          class="ml-4 text-center text-sm text-gray-500 dark:text-gray-400 sm:text-right sm:ml-0"
+        >
+          Laravel v{{ laravelVersion }} (PHP v{{ phpVersion }})
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+.bg-dots-darker {
+  background-image: url("data:image/svg+xml,%3Csvg width='30' height='30' viewBox='0 0 30 30' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M1.22676 0C1.91374 0 2.45351 0.539773 2.45351 1.22676C2.45351 1.91374 1.91374 2.45351 1.22676 2.45351C0.539773 2.45351 0 1.91374 0 1.22676C0 0.539773 0.539773 0 1.22676 0Z' fill='rgba(0,0,0,0.07)'/%3E%3C/svg%3E");
+}
+@media (prefers-color-scheme: dark) {
+  .dark\:bg-dots-lighter {
+    background-image: url("data:image/svg+xml,%3Csvg width='30' height='30' viewBox='0 0 30 30' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M1.22676 0C1.91374 0 2.45351 0.539773 2.45351 1.22676C2.45351 1.91374 1.91374 2.45351 1.22676 2.45351C0.539773 2.45351 0 1.91374 0 1.22676C0 0.539773 0.539773 0 1.22676 0Z' fill='rgba(255,255,255,0.07)'/%3E%3C/svg%3E");
+  }
+}
+</style>
